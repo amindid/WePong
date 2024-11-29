9c430773fdfcbfd65060c2a58d9e2343a5220995
karimch-50 <karimchaouki50@gmail.com>
2024-11-29 02:10:07 +0100

    merge

--- chat/chatservice/chatservice/settings.py
+++ chat/chatservice/chatservice/settings.py
@@ -127,7 +127,7 @@ DATABASES = {
         'NAME': config('POSTGRES_DB'),
         'USER': config('POSTGRES_USER'),
         'PASSWORD': config('POSTGRES_PASSWORD'),
-        'HOST': 'db',
+        'HOST': 'localhost',
         'PORT': '5432',
     }
 }
